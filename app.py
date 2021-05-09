1188b965352df8fe56da774abd4fb2bf6955e435
HayesAJ83 <72214809+HayesAJ83@users.noreply.github.com>
2021-05-09 02:36:06 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
 
 #@app.route('/templates/home.html')
 #def index():
