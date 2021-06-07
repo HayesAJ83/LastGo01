8d66562ad5f7c2bd482823de804a24de9fb533d2
HayesAJ83 <72214809+HayesAJ83@users.noreply.github.com>
2021-06-07 22:11:36 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -53,19 +53,18 @@ def ops():
     url="https://raw.githubusercontent.com/HayesAJ83/LastGo01/master/static/database/Eps4SN.csv"
     df1=pd.read_csv(url)
     df2=df1.sort_values(by=['Operation'], ascending=True)
-    df3=df2['Operation'].dropna()
-    string = df3.str.cat(sep=',')
-    splits = string.split(",")
-    splits = string.split(",")
-    S = set(splits)
-    T = np.array(list(S)).astype(object)
-    U = np.sort(T)
-    V = list(U)
-    W = pd.Series(V)
-    W.name = 'Operation'
-    X = W.iloc[1:]
-    d = X.reset_index(drop=True)
-
+    #df3=df2['Operation'].dropna()
+    #string = df3.str.cat(sep=',')
+    #splits = string.split(",")
+    #S = set(splits)
+    #T = np.array(list(S)).astype(object)
+    #U = np.sort(T)
+    #V = list(U)
+    #W = pd.Series(V)
+    #W.name = 'Operation'
+    #X = W.iloc[1:]
+    #d = X.reset_index(drop=True)
+    seq=df2['Eponym_easy']
     return render_template('ops.html', seq=d)
 
 @app.route('/maps')
