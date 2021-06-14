c2ac05175b71a8e0d1f8cecc4518ff5cddd0d09f
HayesAJ83 <72214809+HayesAJ83@users.noreply.github.com>
2021-06-14 21:06:30 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template(
-        'index.html',
-        data=[{'name':'red'}, {'name':'green'}, {'name':'blue'}])
+    return render_template('index.html',
+        data=[{'name':'red'}, {'name':'green'}, {'name':'blue'}]
+        )
 
-@app.route("/test" , methods=['GET', 'POST'])
+@app.route("/specialties" , methods=['GET', 'POST'])
 def test():
     select = request.form.get('comp_select')
     return(str(select)) # just to see what select is
