da8dbd85a16e4bc2ae09fe21a6789d6b4d253757
HayesAJ83 <72214809+HayesAJ83@users.noreply.github.com>
2021-06-20 00:32:06 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -37,19 +37,19 @@ def alphabet():
     d=df2['Eponym_easy']
 
     if request.method == "POST":
-        if request.values.get('activate-toggle') == 'true':
+        if request.values.get('activate-toggle') == "on":
             print("ON")
-        elif request.values.get('activate-toggle') == 'false':
+        elif request.values.get('activate-toggle') == "off":
             print("OFF")
         else:
             return render_template('alphabet_selectspec.html', specs=specs, names=d)
             
     if request.method == "GET":     #This is what its doing#
         
-        if request.form.get('activate-toggle') == 'true':
+        if request.form.get('activate-toggle') == "on":
             return render_template('alphabet_selectspec.html', specs=specs, names=d)
         
-        elif request.values.get('activate-toggle') == 'false':
+        elif request.values.get('activate-toggle') == "off":
             print("OFF")
         else:
             return render_template('alphabet.html', specs=specs, names=d)
