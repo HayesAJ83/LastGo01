6461977a4a05e30186bfee06db7f4aa369b001ff
HayesAJ83 <72214809+HayesAJ83@users.noreply.github.com>
2021-06-21 22:35:32 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -32,7 +32,8 @@ def alphabet():
     Sp_Z = pd.DataFrame(Sp_Y)
     specs = Sp_Z['Topic']
 
-    SomeValue = request.form.get('SomeValue')
+    if request.method == "POST":
+        SomeValue = request.form.get('SomeValue')
 
     df1=pd.read_csv(url)
     df2=df1.sort_values(by=['Eponym'], ascending=True)
