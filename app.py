54358403cab1e85a8da66aa7496d7e3cd40a2654
HayesAJ83 <72214809+HayesAJ83@users.noreply.github.com>
2021-06-24 16:06:15 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
     
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
     return render_template('index.html')
 
