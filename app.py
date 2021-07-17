fe4ee2c59b7ccac71d723506d78f13ef08470586
HayesAJ83 <72214809+HayesAJ83@users.noreply.github.com>
2021-07-17 22:17:30 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -13,31 +13,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-#@app.route('/alphabet/<mode>', methods=['GET', 'POST'])
-#def alphabet_mode(mode):
-#    session['mode'] = mode
-#    return redirect(url_for('index'))
-#
-#    url="https://raw.githubusercontent.com/HayesAJ83/LastGo01/master/static/database/Eps4SN.csv"
-#    sp1=pd.read_csv(url)
-#    sp2=sp1.sort_values(by=['Topic'], ascending=True)
-#    sp3=sp2['Topic'].dropna()
-#    Sp_string = sp3.str.cat(sep=',')
-#    Sp_splits = Sp_string.split(",")
-#    Sp_S = set(Sp_splits)
-#    Sp_T = np.array(list(Sp_S)).astype(object)
-#    Sp_U = np.sort(Sp_T)
-#    Sp_V = list(Sp_U)
-#    Sp_W = pd.Series(Sp_V).rename('Topic')
-#    Sp_X = Sp_W[1:]
-#    Sp_Y = pd.Series(Sp_X)
-#    Sp_Y.reset_index(inplace=True, drop=True)
-#    Sp_Z = pd.DataFrame(Sp_Y)
-#    specs = Sp_Z['Topic']
-#    df1=pd.read_csv(url)
-#    df2=df1.sort_values(by=['Eponym'], ascending=True)
-#    d=df2['Eponym_easy']
-#    return render_template('alphabet.html', specs=specs, names=d)
 
 
 @app.route('/alphabet', methods=['GET', 'POST'])
