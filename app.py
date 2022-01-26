c71d80838e4de9a5ff70fa87f7125d3fd42fb06b
HayesAJ83 <72214809+HayesAJ83@users.noreply.github.com>
2022-01-26 09:44:23 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -9,29 +9,21 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/specialties', methods=['GET', 'POST'])
-def specialties():
-    return render_template('specialties.html')
-
-
-@app.route('/categories', methods=['GET', 'POST'])
-def categories():
-    return render_template('categories.html')
-
-
-@app.route('/diseases', methods=['GET', 'POST'])
-def diseases():
-    return render_template('diseases.html')
-
-
-@app.route('/journals', methods=['GET', 'POST'])
-def journals():
-    return render_template('journals.html')
-
-
-@app.route('/operations', methods=['GET', 'POST'])
-def ops():
-    return render_template('ops.html')
+#@app.route('/specialties', methods=['GET', 'POST'])
+#def specialties():
+#    return render_template('specialties.html')
+#@app.route('/categories', methods=['GET', 'POST'])
+#def categories():
+#    return render_template('categories.html')
+#@app.route('/diseases', methods=['GET', 'POST'])
+#def diseases():
+#    return render_template('diseases.html')
+#@app.route('/journals', methods=['GET', 'POST'])
+#def journals():
+#    return render_template('journals.html')
+#@app.route('/operations', methods=['GET', 'POST'])
+#def ops():
+#    return render_template('ops.html')
 
 
 @app.route('/maps', methods=['GET', 'POST'])
